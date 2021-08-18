5af17e385e3fc3a45b192fef0da19d248dd006fa
AbrahamAV <abrahamav2002@gmail.com>
2021-08-18 19:29:15 +0300

    Add files via upload

---
+++ Jerry.js
@@ -0,0 +1,36 @@
+function a()
+{
+    document.body.style.backgroundColor= "rgb(240, 20, 20)";
+}
+function b()
+{
+    document.body.style.backgroundColor= "rgb(240, 13, 126)";
+}
+function c()
+{
+    document.body.style.backgroundColor= "rgb(248, 74, 6)";
+}
+function d()
+{
+    document.body.style.backgroundColor= "rgb(226, 123, 39)"
+}
+function e()
+{
+    document.body.style.backgroundColor= "rgb(247, 0, 0)"
+}
+function f()
+{
+    document.body.style.backgroundColor= "rgb(172, 52, 37)"
+}
+function g()
+{
+    document.body.style.backgroundColor= "rgb(133, 42, 1)"
+}
+function h()
+{
+    document.body.style.backgroundColor= "rgb(247, 63, 155)"
+}
+function i()
+{
+    document.body.style.backgroundColor= "rgb(238, 223, 15)"
+}
